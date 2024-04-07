14098db78ffd71a1b087c84832a8e23d88a52972
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-04-07 21:17:03 +0700

    1.3. Hiển thị ảnh dạng slider

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+// Slider Tour Detail
+var swiperSliderThumb = new Swiper(".swiperSliderThumb", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+});
+
+var swiperSliderMain = new Swiper(".swiperSliderMain", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiperSliderThumb,
+  },
+});
+// End Slider Tour Detail
