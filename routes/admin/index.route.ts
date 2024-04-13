08098a74c14af311064c0b2f1abbdc2bdbd6ad8c
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-04-13 22:34:28 +0700

    1. Danh sách tour (Bên Admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,5 +1,6 @@
 import { Express } from "express";
 import { categoryRoutes } from "./category.route";
+import { tourRoutes } from "./tour.route";
 
 import { systemConfig } from "../../config/system";
 
@@ -8,6 +9,8 @@ const adminRoutes = (app: Express): void => {
   const path = `/${systemConfig.prefixAdmin}`;
   
   app.use(`${path}/categories`, categoryRoutes);
+
+  app.use(`${path}/tours`, tourRoutes);
 
 };
 
