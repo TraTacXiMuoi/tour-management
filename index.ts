633d13c6b842f446746c57d8c902bb0ef44051d2
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-04-13 23:06:38 +0700

    2.1. Thêm tour cơ bản (Chưa lưu vào database danh mục)

--- index.ts
+++ index.ts
@@ -16,6 +16,9 @@ app.set("view engine", "pug");
 
 app.use(express.static("public"));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
 app.use(bodyParser.json());
 
